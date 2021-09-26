f8cd1b9660eb2c871e7d2a6a0158d90a14302e84
EC2 Default User <ec2-user@ip-10-0-0-102.ap-northeast-1.compute.internal>
2021-09-26 08:43:08 +0000

    route.rbを編集しました

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,18 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  # deviseのメソッドで「ログインしていないユーザーをログイン画面に送る」メソッド
   def new
+    @tweet = Tweet.new # 新規投稿用の空のインスタンス
   end
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def create
+    
   end
 end
