bc021aa4dcda974d091d94fe378def756736462c
EC2 Default User <ec2-user@ip-10-0-0-102.ap-northeast-1.compute.internal>
2021-09-29 14:01:29 +0000

    Favoriteテーブルに追加したcommentカラムを削除(remove用のマイグレファイル作成、実行)しました

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_29_134436) do
+ActiveRecord::Schema.define(version: 2021_09_29_135802) do
 
   create_table "favorites", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.integer "user_id"
     t.integer "tweet_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.text "comment"
   end
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
