23781891b2086c214e69a7b3a684a14dbcd36dd7
EC2 Default User <ec2-user@ip-10-0-0-102.ap-northeast-1.compute.internal>
2021-09-29 14:04:16 +0000

    db/migrate/20210929134436_add_comment_to_favorites.rbと db/migrate/20210929135802_remove_comment_from_favorites.rbを削除しました

--- db/migrate/20210929134436_add_comment_to_favorites.rb
+++
@@ -1,5 +0,0 @@
-class AddCommentToFavorites < ActiveRecord::Migration[6.0]
-  def change
-    add_column :favorites, :comment, :text
-  end
-end
--- db/migrate/20210929135802_remove_comment_from_favorites.rb
+++
@@ -1,5 +0,0 @@
-class RemoveCommentFromFavorites < ActiveRecord::Migration[6.0]
-  def change
-    remove_column :favorites, :comment, :text
-  end
-end
