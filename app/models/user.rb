c96851d16d297bd5ca4402b25be860f63044a431
EC2 Default User <ec2-user@ip-10-0-0-102.ap-northeast-1.compute.internal>
2021-10-01 15:43:12 +0000

    app/models/user.rb 14~21行目を追加しました
    app/models/relationship.rb 2,3行目を追加しました

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,15 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :favorite_tweets, through: :favorites, source: :tweet
   has_many :comments
+  
+  
+  #フォローする側のUserから見て、フォローされる側のUserを(中間テーブルを介して)集める。なので親はfollowing_id(フォローする側)
+  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
+  # 中間テーブルを介して「follower」モデルのUser(フォローされた側)を集めることを「followings」と定義
+  has_many :followings, through: :active_relationships, source: :follower
+  
+  #フォローされる側のUserから見て、フォローしてくる側のUserを(中間テーブルを介して)集める。なので親はfollower_id(フォローされる側)
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
+  # 中間テーブルを介して「following」モデルのUser(フォローする側)を集めることを「followers」と定義
+  has_many :followers, through: :passive_relationships, source: :following
 end
