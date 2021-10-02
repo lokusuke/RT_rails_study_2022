8dace8a6d5ae821fbf23b4afaaf602eca660bef2
EC2 Default User <ec2-user@ip-10-0-0-102.ap-northeast-1.compute.internal>
2021-10-02 12:32:40 +0000

    app/models/user.rb 23~26行目を追加しました(userがフォローされているかの判断用のメソッド導入)
    app/models/tweet.rb 6,7行目に追加しました(ファボの判断文について説明を加えた)

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,6 +2,9 @@ class Tweet < ApplicationRecord
     belongs_to :user
     has_many :favorites
     has_many :comments
+    
+    #あるtweetに対して、(自分がログインしている)userがお気に入り登録しているかどうかをみている
+    #もっと正確にいうと、あるtweet_idに対してお気に入り登録しているuser_idがfavoritesテーブルに存在しているかどうかをみている
     def favorited_by?(user)
         favorites.where(user_id: user.id).exists?
     end
